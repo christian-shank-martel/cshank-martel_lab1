b5c5910ecd274a9226c2fcd393363f663394baaf
cshank-martel <christian.shank.martel@gmail.com>
2023-02-16 17:16:15 -0500

    Version1.2

--- lab1.py
+++ lab1.py
@@ -7,18 +7,24 @@ from datetime import date
 
 def age_finder(born):
     today = date.today()
+    
     age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
- 
+
+
     return age
 
 def Helloworld():
     print("hello world!")
 
 if __name__ == '__main__':
-    year_born = int(input("What year were you born?: "))
-    month_born = int(input("What month were you born?: "))
-    day_born = int(input("What day were you born?: "))
-    age = age_finder(date(year_born,month_born,day_born))
-    print("\nyou are ",age," years old")
+    year_born = int((input("What year were you born?: ")))
+    month_born = int((input("What month were you born?: ")))
+    day_born = int((input("What day were you born?: ")))
+    try:
+        age = age_finder(date(year_born,month_born,day_born))
+        print("\nyou are ",age," years old")
+    except:
+        print("Please enter only int type")
 
     Helloworld()
+
