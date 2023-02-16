f5d862d7a430ed4d61753e0b5bbf5b3cbf01015a
christian-shank-martel <61027159+christian-shank-martel@users.noreply.github.com>
2023-02-15 22:11:05 -0500

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#Author: christian shank-martel
+#Date:feb 15th 2023
+#Program: Lab1.py
+
+from datetime import date
+
+def age_finder(born):
+    today = date.today()
+    age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+ 
+    return age
+
+if __name__ == '__main__':
+    year_born = int(input("What year were you born?: "))
+    month_born = int(input("What month were you born?: "))
+    day_born = int(input("What day were you born?: "))
+    age = age_finder(date(year_born,month_born,day_born))
+    print("\nyou are ",age," years old")
